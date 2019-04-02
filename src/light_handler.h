a0fcc76bfe4ba1b1d570fbb050b1c2a9bddd1674
Oskiboy <oskar.oestby@gmail.com>
2019-04-02 10:55:21 +0200

    Adds a function to handle lights

---
+++ src/light_handler.h
@@ -0,0 +1,16 @@
+/**
+ * @file
+ * 
+ */
+#ifndef LIGHT_HANDLER_H
+#define LIGHT_HANDLER_H
+
+/**
+ * @brief This function should light all the button lights and floor light.
+ * 
+ * 
+ */
+void update_lights(void);
+
+
+#endif //LIGHT_HANDLER_H
