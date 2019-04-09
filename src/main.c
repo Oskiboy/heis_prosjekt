33b3d056d6aa243244e2350150934b2e4d070640
Oskiboy <oskar.oestby@gmail.com>
2019-04-09 09:39:29 +0200

    Adds code to main

--- src/main.c
+++ src/main.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
+#include "fsm.h"
+#include "queue_handler.h"
+
+FSM_MODULE(fsm_m);
+QUEUE_MODULE(queue_m);
 
 int main() {
-    printf("Hello, world!\n");
+    printf("Elevator starting...\n");
+    run_fsm(&fsm_m, &queue_m);
+    printf("Elevator stopping!\n");
     return 0;
 }
