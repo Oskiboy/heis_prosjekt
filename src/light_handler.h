0358e2c44afacc380e12476f3338b5f6764246f1
Oskiboy <oskar.oestby@gmail.com>
2019-04-09 13:45:13 +0200

    Adds documentation to the light handler

--- src/light_handler.h
+++ src/light_handler.h
@@ -1,17 +1,22 @@
 /**
- * @file light_handler.h
+ * @file
+ * @brief Functions for handling light.
  * 
  */
 #ifndef LIGHT_HANDLER_H
 #define LIGHT_HANDLER_H
 
 /**
- * @brief This function should light all the button lights and floor light.
- * 
- * 
+ * @brief Turns on all order button lights and floor light.
+ * Every time this function is called it will check all buttons and the floor sensor
+ * and light all the required lights.
  */
 void update_lights(void);
 
+/**
+ * @brief Clears all order lights for the current floor.
+ * When this function is called it clears all orders for the corresponding floor.
+ */
 void clear_order_lights(void);
 
 #endif //LIGHT_HANDLER_H
