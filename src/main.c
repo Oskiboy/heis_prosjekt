62274760d29de95524f034c9fa135431dfaefc12
Oskiboy <oskar.oestby@gmail.com>
2019-04-09 09:43:29 +0200

    Adds elevator initialisation

--- src/main.c
+++ src/main.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include "fsm.h"
 #include "queue_handler.h"
+#include "elev.h"
 
 FSM_MODULE(fsm_m);
 QUEUE_MODULE(queue_m);
 
 int main() {
+    elev_init();
     printf("Elevator starting...\n");
     run_fsm(&fsm_m, &queue_m);
     printf("Elevator stopping!\n");
